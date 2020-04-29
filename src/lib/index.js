dc6c1e49e1e0f4cef58563d5174b0df41619d426
yangjinkai <yangjinkai@yangjinkaideMacBook-Pro.local>
2020-04-29 10:05:59 +0800

    fix array bugs

--- src/lib/index.js
+++ src/lib/index.js
@@ -12,7 +12,6 @@
         myParentEle.style.height = '100%';
         myParentEle.style.overflow = 'auto';
         container.appendChild(myParentEle)
-        
         var contentBox = document.createElement('div');
         contentBox.className = 'mobile-live-message-content-box';
         myParentEle.appendChild(contentBox);
@@ -22,13 +21,11 @@
         myParentEle.addEventListener('touchend', (e) => {
             var bottomLen = Math.abs(myParentEle.scrollTop - (contentBox.offsetHeight - myParentEle.offsetHeight));
             if (bottomLen > 30) {
-                console.log('停止自动滚动底部')
                 this.stopScroll = true;
                 setTimeout(() => {
                     this.stopScroll = false;
                 }, 50000)
             } else {
-                console.log('开启滚动')
                 this.stopScroll = false;
             }
         })
@@ -41,7 +38,10 @@
         if (data instanceof Array) {
             for (let index = 0; index < data.length; index++) {
                 const mesItem = data[index];
-                let content = mesItem.text;
+                let content = mesItem;
+                if (mesItem instanceof Object) {
+                    content = mesItem.text;
+                }
                 if (this.options.format) {
                     content = this.options.format(mesItem);
                 }
@@ -56,7 +56,6 @@
         } else {
             str = `<div class="mobile-live-message-item-text">${data || ''}</div>`
         }
-        var par = document.querySelector('.mobile-live-message-box');
         div.innerHTML = str;
         this.parentElement.appendChild(div);
         if (this.stopScroll) {
